dcef563e708062280e256707307e5fa6fc707776
anurag-512 <anuragsharan51@gmail.com>
2024-05-29 09:46:58 +0530

    section-4 com. done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,12 @@
+let menubox = document.querySelector(".menubox");
+let navitems = document.querySelector(".nav_items");
+
+menubox.addEventListener("click", function () {
+    navitems.classList.toggle("active");
+});
+
+
+
 $('.slick-slider').slick({
     dots: false,
     infinite: true,
